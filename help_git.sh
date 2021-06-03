ec25b435a2e643ebb343b50867400860f7ebc265
dirigar <dini@bk.ru>
2021-06-03 09:53:15 +0300

    ликбез по git

---
+++ help_git.sh
@@ -0,0 +1,30 @@
+
+
+git --version  #// version
+
+
+git config --global --list
+
+# config git
+
+git config --global user.name "dirigar"
+
+git config --global user.email "dini@bk.ru"
+
+git config --global --list # показать настройки
+
+### создадим локальный репозитарий "test1"
+
+pushd /e/yandexdisk_dirigar/YandexDisk/script/git
+
+mkdir test1
+
+cd test1
+
+git init 
+
+# добавление фала в будущий commint
+
+git add --all
+
+
