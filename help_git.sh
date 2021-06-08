7aa916e8291f7a00f15d9f6c8fc712d92cfc27ad
dirigar <dini@bk.ru>
2021-06-08 16:22:55 +0300

    help git

--- help_git.sh
+++ help_git.sh
@@ -20,22 +20,31 @@ pushd /e/yandexdisk_dirigar/YandexDisk/script/git
 mkdir test1
 
 cd test1
-
+# инициализация репозитория
 git init 
 
-# добавление файлов в будущий commint
+# добавление файлов в будущий commint, подготовить, а точнее, проиндексировать
 
 git add --all
-
-# добавление файла в commit
-
 git add ./help_git.sh
+
+# удаление файла из отслеживаемых, т.е. удаление индекса
+
+git rm --cached no_repositary.txt
+
 
 # создаем commit обязательно с комментарием
 
 git commit -m "ликбез по git"
     
     # [master (root-commit) ec25b43] ликбез по git
-    # 1 file changed, 30 insertions(+)
+    # 1 file changed, 30 insertions(+)              # 1 файл изменен , 30 вставок
     # create mode 100644 help_git.sh
+
+# статус репозитория
+
+git status
+
+
+
 
